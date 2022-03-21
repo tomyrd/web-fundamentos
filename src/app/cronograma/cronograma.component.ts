0407d3a41316cfc2c38b2d961ded8153f51b9a06
mellamopepino <delfinagarciav@gmail.com>
2022-03-20 23:30:42 -0300

    Agregar cronograma 1C2022
    - Cambiar diseño de cronograma.
    - Agregar el cronograma para 1C2022.

--- src/app/cronograma/cronograma.component.ts
+++ src/app/cronograma/cronograma.component.ts
@@ -13,6 +13,7 @@ export class CronogramaComponent implements OnInit {
   fecha: string;
   actual = 0;
   cronogramaURL: any;
+  hitosTipos: Object;
 
   constructor(
     public datepipe: DatePipe,
@@ -23,22 +24,415 @@ export class CronogramaComponent implements OnInit {
 
     this.fecha = this.datepipe.transform(new Date(), 'yyyyMMdd');
     this.cronograma = [
-      {numero: "1", fechaInicio: "20210906", fechaFin: "20210912", tema: "Introducción a la algoritmia y a la programación.", hitos: []},
-      {numero: "2", fechaInicio: "20210913", fechaFin: "20210919", tema: "Codificación, compilación y ejecución. Estructuras de control.", hitos: []},
-      {numero: "3", fechaInicio: "20210920", fechaFin: "20210926", tema: "Funciones y procedimientos. Pasaje de parámetros por valor.", hitos: []},
-      {numero: "4", fechaInicio: "20210927", fechaFin: "20211003", tema: "Pasaje de parámetros por referencia. Buenas prácticas de programación.", hitos: []},
-      {numero: "5", fechaInicio: "20211004", fechaFin: "20211010", tema: "Bibliotecas. Strings. Registros.", hitos: ["Presentación TP Parte 1"]},
-      {numero: "6", fechaInicio: "20211011", fechaFin: "20211017", tema: "Vectores y matrices. Búsqueda lineal.", hitos: []},
-      {numero: "7", fechaInicio: "20211018", fechaFin: "20211024", tema: "Eliminación de elementos. Estructuras anidadas.", hitos: []},
-      {numero: "8", fechaInicio: "20211025", fechaFin: "20211031", tema: "Métodos de ordenamiento. Inserción ordenada.", hitos: ["Entrega TP Parte 1","Presentación TP Parte 2"]},
-      {numero: "9", fechaInicio: "20211101", fechaFin: "20211107", tema: "Unión, intersección y diferencia. Recursividad.", hitos: []},
-      {numero: "10", fechaInicio: "20211108", fechaFin: "20211114", tema: "Recursividad. Busqueda binaria", hitos: ["Reentrega TP Parte 1"]},
-      {numero: "11", fechaInicio: "20211115", fechaFin: "20211121", tema: "Repaso.", hitos: ["Parcial"]},
-      {numero: "12", fechaInicio: "20211122", fechaFin: "20211128", tema: "Archivos de texto. Acceso secuencial", hitos: ["Entrega TP Parte 2","Presentación TP Parte 3"]},
-      {numero: "13", fechaInicio: "20211129", fechaFin: "20211205", tema: "Archivos de texto. Acceso secuencial", hitos: []},
-      {numero: "14", fechaInicio: "20211206", fechaFin: "20211212", tema: "Mesa de debate con otros docentes. Algo1 en otro lenguaje.", hitos: ["Reentrega TP Parte 2","Entrega TP Parte 3"]},
-      {numero: "15", fechaInicio: "20211213", fechaFin: "20211219", tema: "Retrospectiva.", hitos: ["1er recuperatorio parcial"]},
-      {numero: "16", fechaInicio: "20211220", fechaFin: "20211226", tema: "", hitos: ["Reentrega TP Parte 3"]},
+      {
+        numero: 1,
+        fechaInicio: 20220321,
+        fechaFin: 20220327,
+        teorica: {
+          fecha: new Date(2022, 2, 22),
+          temas: [
+            'Introducción a la materia',
+            'Algoritmos',
+            'Variables, constantes y literales',
+            'Tipos de dato y operadores',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 2, 24),
+          temas: [],
+          hitos: [],
+          esFeriado: true,
+        },
+      },
+      {
+        numero: 2,
+        fechaInicio: 20220328,
+        fechaFin: 20220403,
+        teorica: {
+          fecha: new Date(2022, 2, 29),
+          temas: [
+            'Datos de entrada y salida',
+            'Pre y post condiciones',
+            'Caja negra y caja blanca',
+            'Codigicación, compilación y ejecución',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 2, 31),
+          temas: [
+            'Pre y post condiciones',
+            'Estructuras de control',
+            'Elaboración de algoritmos',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+      },
+      {
+        numero: 3,
+        fechaInicio: 20220404,
+        fechaFin: 20220410,
+        teorica: {
+          fecha: new Date(2022, 3, 5),
+          temas: [
+            'Funciones y Procedimientos',
+            'Pasaje de parámetros por valor y por referencia',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 3, 7),
+          temas: [
+            'Funciones y Procedimientos',
+            'Pasaje de parámetros por valor y por referencia',
+          ],
+          hitos: [
+            {
+              titulo: 'Presentación TP1',
+              tipo: 'presentacion-tp',
+            }
+          ],
+          esFeriado: false,
+        },
+      },
+      {
+        numero: 4,
+        fechaInicio: 20220411,
+        fechaFin: 20220417,
+        teorica: {
+          fecha: new Date(2022, 3, 12),
+          temas: [
+            'Pasaje de parámetros por referencia',
+            'Buenas prácticasd de programación',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 3, 14),
+          temas: [],
+          hitos: [],
+          esFeriado: true,
+        },
+      },
+      {
+        numero: 5,
+        fechaInicio: 20220418,
+        fechaFin: 20220424,
+        teorica: {
+          fecha: new Date(2022, 3, 19),
+          temas: [
+            'Bibliotecas',
+            'Registros',
+            'Strings',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 3, 21),
+          temas: [
+            'Bibliotecas',
+            'Registros',
+            'Strings',
+          ],
+          hitos: [
+            {
+              titulo: 'Entrega TP1',
+              tipo: 'entrega-tp',
+            },
+          ],
+          esFeriado: false,
+        },
+      },
+      {
+        numero: 6,
+        fechaInicio: 20220425,
+        fechaFin: 20220501,
+        teorica: {
+          fecha: new Date(2022, 3, 26),
+          temas: [
+            'Vectores y matrices',
+            'Búsqueda lineal',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 3, 28),
+          temas: [
+            'Vectores y matrices',
+            'Búsqueda lineal',
+          ],
+          hitos: [
+            {
+              titulo: 'Presentación TP2',
+              tipo: 'presentacion-tp',
+            },
+            
+          ],
+          esFeriado: false,
+        },
+      },
+      {
+        numero: 7,
+        fechaInicio: 20220502,
+        fechaFin: 20220508,
+        teorica: {
+          fecha: new Date(2022, 4, 3),
+          temas: [
+            'Eliminación de elementos',
+            'Estructuras anidadas',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 4, 5),
+          temas: [
+            'Eliminación de elementos',
+            'Estructuras anidadas',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+      },
+      {
+        numero: 8,
+        fechaInicio: 20220509,
+        fechaFin: 20220515,
+        teorica: {
+          fecha: new Date(2022, 4, 10),
+          temas: [
+            'Métodos de ordenamiento',
+            'Inserción ordenada',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 4, 12),
+          temas: [
+            'Métodos de ordenamiento',
+            'Métodos de búsqueda',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+      },
+      {
+        numero: 9,
+        fechaInicio: 20220516,
+        fechaFin: 20220522,
+        teorica: {
+          fecha: new Date(2022, 4, 17),
+          temas: [
+            'Recursividad',
+            'Búsqueda binaria',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 4, 19),
+          temas: [
+            'Recursividad',
+            'Búsqueda binaria',
+          ],
+          hitos: [
+            {
+              titulo: 'Entrega TP2',
+              tipo: 'entrega-tp',
+            },
+            
+          ],
+          esFeriado: false,
+        },
+      },
+      {
+        numero: 10,
+        fechaInicio: 20220523,
+        fechaFin: 20220529,
+        teorica: {
+          fecha: new Date(2022, 4, 24),
+          temas: [
+            'Recursividad',
+            'Unión, intersección y diferencia',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 4, 26),
+          temas: [
+            'Recursividad',
+            'Unión, intersección y diferencia',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+      },
+      {
+        numero: 11,
+        fechaInicio: 20220530,
+        fechaFin: 20220605,
+        teorica: {
+          fecha: new Date(2022, 4, 31),
+          temas: [],
+          hitos: [
+            
+            {
+              titulo: 'Repaso',
+              tipo: 'otros',
+            },
+          ],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 5, 2),
+          temas: [],
+          hitos: [
+            {
+              titulo: 'Parcial',
+              tipo: 'parcial',
+            },
+            {
+              titulo: 'Reentrega TP2',
+              tipo: 'entrega-tp',
+            },
+            
+          ],
+          esFeriado: false,
+        },
+      },
+      {
+        numero: 12,
+        fechaInicio: 20220606,
+        fechaFin: 20220612,
+        teorica: {
+          fecha: new Date(2022, 5, 7),
+          temas: [
+            'Archivos de texto',
+            'Accesso secuencial',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 5, 9),
+          temas: [
+            'Archivos de texto',
+            'Accesso secuencial',
+          ],
+          hitos: [
+            {
+              titulo: 'Presentación TP3',
+              tipo: 'presentacion-tp',
+            }
+          ],
+          esFeriado: false,
+        },
+      },
+      {
+        numero: 13,
+        fechaInicio: 20220613,
+        fechaFin: 20220619,
+        teorica: {
+          fecha: new Date(2022, 5, 14),
+          temas: [
+            'Procesamiento de archivos',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 5, 16),
+          temas: [
+            'Procesamientos de archivos',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+      },
+      {
+        numero: 14,
+        fechaInicio: 20220620,
+        fechaFin: 20220626,
+        teorica: {
+          fecha: new Date(2022, 5, 21),
+          temas: [
+            'Ejercicios integradores',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 5, 23),
+          temas: [
+            'Ejercicios integradores',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+      },
+      {
+        numero: 15,
+        fechaInicio: 20220627,
+        fechaFin: 20220703,
+        teorica: {
+          fecha: new Date(2022, 5, 28),
+          temas: [
+            'Mesa de debate con otros docentes',
+          ],
+          hitos: [],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 5, 30),
+          temas: [
+            'Algo1 en otro lenguaje',
+          ],
+          hitos: [
+            {
+              titulo: 'Entrega TP3',
+              tipo: 'entrega-tp',
+            }
+          ],
+          esFeriado: false,
+        },
+      },
+      {
+        numero: 16,
+        fechaInicio: 20220704,
+        fechaFin: 20220710,
+        teorica: {
+          fecha: new Date(2022, 6, 5),
+          temas: [],
+          hitos: [
+            {
+              titulo: '1er recuperatorio parcial',
+              tipo: 'parcial',
+            },
+          ],
+          esFeriado: false,
+        },
+        practica: {
+          fecha: new Date(2022, 6, 7),
+          temas: [],
+          hitos: [
+            {
+              titulo: 'Retrospectiva',
+              tipo: 'otros',
+            },
+          ],
+          esFeriado: false,
+        },
+      },
     ];
   }
 
