389ea5eb66ac802a27dea735d3f2770b31c2e76d
Tomás Rodríguez Dala <tomas.rodriguez@lambdaclass.com>
2024-03-06 23:53:33 -0300

    Update dev server URL and center content in informacion component

--- src/app/informacion/informacion.component.ts
+++ src/app/informacion/informacion.component.ts
@@ -28,15 +28,15 @@ export class InformacionComponent implements OnInit {
 		this.regimenCursada = this.restApi.getRegimenPdf();
 
 		var encuestas = [
-			{ nombre: '1C 2021', url: '/assets/docs/encuestas/encuesta_2021_1C.html'},
-			{ nombre: '2C 2020', url: '/assets/docs/encuestas/encuesta_2020_2C.html'},
-			{ nombre: '1C 2020', url: '/assets/docs/encuestas/encuesta_2020_1C.html'},
-			{ nombre: '2C 2019', url: '/assets/docs/encuestas/encuesta_2019_2C.html'},
-			{ nombre: '1C 2019', url: '/assets/docs/encuestas/encuesta_2019_1C.html'},
-			{ nombre: '2C 2018', url: '/assets/docs/encuestas/encuesta_2018_2C.html'},
-			{ nombre: '1C 2018', url: '/assets/docs/encuestas/encuesta_2018_1C.html'},
-			{ nombre: '2C 2017', url: '/assets/docs/encuestas/encuesta_2017_2C.html'},
-			{ nombre: '1C 2017', url: '/assets/docs/encuestas/encuesta_2017_1C.html'},
+			// { nombre: '1C 2021', url: '/assets/docs/encuestas/encuesta_2021_1C.html'},
+			// { nombre: '2C 2020', url: '/assets/docs/encuestas/encuesta_2020_2C.html'},
+			// { nombre: '1C 2020', url: '/assets/docs/encuestas/encuesta_2020_1C.html'},
+			// { nombre: '2C 2019', url: '/assets/docs/encuestas/encuesta_2019_2C.html'},
+			// { nombre: '1C 2019', url: '/assets/docs/encuestas/encuesta_2019_1C.html'},
+			// { nombre: '2C 2018', url: '/assets/docs/encuestas/encuesta_2018_2C.html'},
+			// { nombre: '1C 2018', url: '/assets/docs/encuestas/encuesta_2018_1C.html'},
+			// { nombre: '2C 2017', url: '/assets/docs/encuestas/encuesta_2017_2C.html'},
+			// { nombre: '1C 2017', url: '/assets/docs/encuestas/encuesta_2017_1C.html'},
 		];
 		this.encuestas = encuestas;
 		this.selectedSurvey = encuestas[0].url;
