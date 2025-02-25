337ad13383e05f29566ff6076586eebfe821b036
mbmassone <mbmassone@fi.uba.ar>
2025-02-25 19:22:04 -0300

    update docentes

--- src/app/docentes/docentes.component.ts
+++ src/app/docentes/docentes.component.ts
@@ -18,17 +18,18 @@ export class DocentesComponent implements OnInit {
       { name: "Agus F.", url1: "assets/img/docentes/agusF1.jpg", url2: "assets/img/docentes/agusF2.jpg" },
       { name: "Bauti", url1: "assets/img/docentes/bauti1.jpg", url2: "assets/img/docentes/bauti2.jpg" },
       { name: "Berni", url1: "assets/img/docentes/berni1.jpg", url2: "assets/img/docentes/berni2.jpg" },
-      { name: "Dani V.", url1: "assets/img/docentes/dani1.jpeg", url2: "assets/img/docentes/dani2.jpeg" },
-      { name: "Danny D.", url1: "assets/img/docentes/danny1.jpg", url2: "assets/img/docentes/danny2.jpg" },
-      { name: "Flor", url1: "assets/img/docentes/flor1.jpg", url2: "assets/img/docentes/flor2.jpg" },
+      { name: "Danny", url1: "assets/img/docentes/danny1.jpg", url2: "assets/img/docentes/danny2.jpg" },
+      { name: "Enzo", url1: "assets/img/docentes/enzo1.jpeg", url2: "assets/img/docentes/enzo2.jpeg" },
+      { name: "Gilbert", url1: "assets/img/docentes/gilbert1.jpeg", url2: "assets/img/docentes/gilbert2.jpeg" },
       { name: "Juampi", url1: "assets/img/docentes/juampi1.jpg", url2: "assets/img/docentes/juampi2.jpg" },
       { name: "Mariano", url1: "assets/img/docentes/mariano1.jpeg", url2: "assets/img/docentes/mariano2.jpeg" },
+      { name: "Mar", url1: "assets/img/docentes/mar1.jpeg", url2: "assets/img/docentes/mar2.jpeg" },
       { name: "Martu", url1: "assets/img/docentes/martu1.jpg", url2: "assets/img/docentes/martu2.jpg" },
-      { name: "Matías", url1: "assets/img/docentes/matias1.jpeg", url2: "assets/img/docentes/matias2.jpeg" },
+      { name: "May", url1: "assets/img/docentes/may1.jpeg", url2: "assets/img/docentes/may2.jpeg" },
+      { name: "Pipe", url1: "assets/img/docentes/pipe1.jpeg", url2: "assets/img/docentes/pipe2.jpeg" },
       { name: "Solci", url1: "assets/img/docentes/sol1.jpg", url2: "assets/img/docentes/sol2.jpg" },
       { name: "Tomi", url1: "assets/img/docentes/tomi1.jpg", url2: "assets/img/docentes/tomi2.jpg" },
       { name: "Tute", url1: "assets/img/docentes/tute1.jpeg", url2: "assets/img/docentes/tute2.jpeg" },
-      { name: "Valen", url1: "assets/img/docentes/valen1.jpeg", url2: "assets/img/docentes/valen2.jpeg" },
       { name: "Yoel", url1: "assets/img/docentes/yoel1.jpeg", url2: "assets/img/docentes/yoel2.jpeg" }
     ], 5);
   }
